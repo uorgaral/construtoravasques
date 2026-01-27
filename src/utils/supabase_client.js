4e61eb19e449732f7c40ea394097d5d2730b5b67
uorgaral <laram.grourondolfo@gmail.com>
2026-01-27 11:45:43 -0300

    Erros de deploy 9999

--- src/utils/supabase_client.js
+++ src/utils/supabase_client.js
@@ -1,10 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
 
-import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
-        
