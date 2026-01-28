02a114fc51854748272f5ef80a59740b9f80b156
uorgaral <laram.grourondolfo@gmail.com>
2026-01-28 08:21:50 -0300

    Erros de deploy infinitos

--- src/utils/supabase_client.js
+++ src/utils/supabase_client.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseClient = createClient(supabaseUrl, supabaseKey)
 
-export default supabase
+export default supabaseClient;
+
